8f031a23c16bade26818c1c4db4ffa89d9c8ebe2
Taloreff-hls <talo@moveohls.com>
2024-09-18 15:47:39 +0300

    test(pokemons): add tests for Pokémon random route

--- server.ts
+++ server.ts
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import pokemonRoutes from "./api/route/pokemons.route";
 
 dotenv.config();
 
-const app = express();
+export const app = express();
 const port = process.env.PORT || 4000;
 
 if (process.env.NODE_ENV === "production") {
@@ -21,6 +24,30 @@ if (process.env.NODE_ENV === "production") {
 app.use(cors());
 app.use(express.json());
 
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Pokémon API",
+      version: "1.0.0",
+      description: "API for Pokémon Web game",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+        description: "Local Development Server",
+      },
+    ],
+  },
+  apis: ["./api/route/*.ts"],
+};
+
+// Initialize Swagger docs
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use("/pokemons", pokemonRoutes);
+
 app.get("/", (req, res) => {
   res.send("Pokedex backend is running!");
 });
@@ -29,6 +56,8 @@ app.get("/**", (req, res) => {
   res.sendFile(path.resolve("public/index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
